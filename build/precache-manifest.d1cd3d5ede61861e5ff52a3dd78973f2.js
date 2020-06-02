c199965e73c56159a2bae097e6d8cd73add70169
phtf92 <phtf92@gmail.com>
2020-06-02 11:39:06 -0300

    Adding to Git Hub Pages

---
+++ build/precache-manifest.d1cd3d5ede61861e5ff52a3dd78973f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb0689ee6ae54da3b9439d549b37d50",
+    "url": "/react-redux-sw/index.html"
+  },
+  {
+    "revision": "d8d09d8a6c812b35c66e",
+    "url": "/react-redux-sw/static/css/main.43aa2853.chunk.css"
+  },
+  {
+    "revision": "7c5f9ed709aff1f474fb",
+    "url": "/react-redux-sw/static/js/2.76849c38.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/react-redux-sw/static/js/2.76849c38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8d09d8a6c812b35c66e",
+    "url": "/react-redux-sw/static/js/main.128c2be8.chunk.js"
+  },
+  {
+    "revision": "c1c4d4cfa9a271eeca9c",
+    "url": "/react-redux-sw/static/js/runtime-main.fa141ba9.js"
+  }
+]);
